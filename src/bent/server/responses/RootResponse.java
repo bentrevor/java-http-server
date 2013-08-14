955fed7de85f4cb431f3bb268fb7c7840e767963
Ben Trevor <benjamin.trevor@gmail.com>
2013-08-14 14:30:06 -0500

    Add public directory links to root path

--- src/bent/server/responses/RootResponse.java
+++ src/bent/server/responses/RootResponse.java
@@ -3,6 +3,10 @@ package bent.server.responses;
 import bent.server.HttpRequest;
 import bent.server.HttpResponse;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class RootResponse extends HttpResponse {
     public RootResponse() {
     }
@@ -10,6 +14,23 @@ public class RootResponse extends HttpResponse {
     public void buildResponse(HttpRequest request) {
         if (request.getMethod().equals("GET")) {
             setStatusLine("HTTP/1.1 200 OK");
+            setBody(homePage());
         }
+    }
+
+    private String homePage() {
+        StringBuilder page = new StringBuilder();
+
+        page.append("<!DOCTYPE html><html><head></head><body>");
+        page.append("<a href=\"/file1\">file1</a>");
+        page.append("<a href=\"/file2\">file2</a>");
+        page.append("<a href=\"/image.gif\">image.gif</a>");
+        page.append("<a href=\"/image.jpeg\">image.jpeg</a>");
+        page.append("<a href=\"/image.png\">image.png</a>");
+        page.append("<a href=\"/text-file.txt\">text-file.txt</a>");
+        page.append("<a href=\"/partial_content.txt\">partial_content.txt</a>");
+        page.append("</body></html>");
+
+        return page.toString();
     }
 }
--- src/tests/responses/RootResponseTest.java
+++ src/tests/responses/RootResponseTest.java
@@ -9,6 +9,7 @@ import org.junit.runners.JUnit4;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 @RunWith(JUnit4.class)
 public class RootResponseTest {
@@ -19,5 +20,20 @@ public class RootResponseTest {
         response.buildResponse(request);
 
         assertThat(response.statusLine, is("HTTP/1.1 200 OK"));
+    }
+
+    @Test
+    public void itShowsTheDirectoryLinksInHomeHtml() {
+        HttpRequest request = new HttpRequest("GET / HTTP/1.1\r\n\r\n");
+        HttpResponse response = new RootResponse();
+        response.buildResponse(request);
+
+        assertThat(response.body, containsString("<a href=\"/file1\">file1</a>"));
+        assertThat(response.body, containsString("<a href=\"/file2\">file2</a>"));
+        assertThat(response.body, containsString("<a href=\"/image.gif\">image.gif</a>"));
+        assertThat(response.body, containsString("<a href=\"/image.jpeg\">image.jpeg</a>"));
+        assertThat(response.body, containsString("<a href=\"/image.png\">image.png</a>"));
+        assertThat(response.body, containsString("<a href=\"/text-file.txt\">text-file.txt</a>"));
+        assertThat(response.body, containsString("<a href=\"/partial_content.txt\">partial_content.txt</a>"));
     }
 }
